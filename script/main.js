bb0ea76c58fe1aa07612553afc37eb9b9116baab
Филипченко Мария Витальевна <filipchanka@sc.softclub.by>
2020-03-10 11:41:39 +0300

    create sending message and add time for it

--- script/main.js
+++ script/main.js
@@ -15,11 +15,31 @@ function addMessage () {
     let sendMessageButton = document.getElementById('send-message');
     let inputMessageArea = document.getElementById('input-message');
    
-    if (inputMessageArea !== '') {
+    
+   
+    if (inputMessageArea != '') {
         sendMessageButton.addEventListener('click', ()=> {
             let createMesage = document.createElement('li');
-            createMesage.className = 'from-message';
+            createMesage.className = 'message from-message';
+            let messageValue =  inputMessageArea.value;
+            let time = new Date().toLocaleString('ru-RU', {
+                hour: "numeric", 
+                minute: "numeric"
+            })
+             
+            createMesage.innerHTML = `
+            
+                <div class="from-message-text">
+                    <p>${messageValue}</p>
+                    <p class="from-time" id="time">${time}</p>
+                </div>
+            `
             console.log('add-message');
+
+           
+
+            document.getElementById('message-items-wrapper').appendChild(createMesage);
+            createMesage.scrollIntoView();
         })
     }
 }
